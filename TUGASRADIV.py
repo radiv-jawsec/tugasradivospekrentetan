acc8fd1ac899010f935aab264822d19ebeac34a6
radiv-jawsec <71246695+radiv-jawsec@users.noreply.github.com>
2023-09-02 14:42:46 +0700

    Add files via upload

---
+++ TUGASRADIV.py
@@ -0,0 +1,24 @@
+JAWABAN NO 2 (GANJIL)
+Celcius ke Fahrenheit
+Fahrenheit ke Celcius
+Masukkan Suhu (Celcius) = 30
+Hasil Celcius ke Fahrenheit = 86.0
+
+Celcius Ke Fahrenheit
+Fahrenhait ke Celcius
+Masukkan Suhu (fahrenheit) = 5
+Hasil Fahrenheit ke Celcius = -15.0
+
+
+JAWABAN NO 2 (GANJIL)
+print("1. Celcius ke Fahrenheit")
+print("2. Fahrenheit e Celcius")
+choose = input("Pilih Opsi : ")
+if choose =="1":
+     temp = input("Masukkan Suhu (Celcius) : ")
+     calc = (int("temp") * 9/5) + 32
+     print("Hasil Celcius ke Fahrenheit : str(calc))
+           elif choose == "2":
+           temp = input("Masukkan Suhu (fahrenheit) :")
+           calc = (int(temp) - 32) * 5/9
+           print("Hasil Fahrenheit ke Celcius : ' str(calc)")
